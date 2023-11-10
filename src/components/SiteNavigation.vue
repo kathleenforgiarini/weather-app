06111ca0f1bf96f71317c6cb57d2b53a61a7e905
kathleenforgiarini <kath.for199@gmail.com>
2023-11-09 23:00:20 -0500

    adding cities search and results

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -44,7 +44,7 @@
           <h2 class="text-2xl">Removing a city</h2>
           <p>
             If you no longer wish to track a city, simply select the city within
-            the home page. At the bottom of the page, there will be am option to
+            the home page. At the bottom of the page, there will be an option to
             delete the city.
           </p>
         </div></BaseModal
